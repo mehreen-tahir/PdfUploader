5624d6c3d7d09fcb4f6bfac0ed8605f6f1ff6701
Mehreen Tahir <mehreen.tahir@square63.com>
2016-10-24 01:59:46 +0500

    Route: Added routes for document uploading

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :documents, only: [:new, :create]
+
+  root 'documents#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
